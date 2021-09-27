cf8f5de2cf476edafac62ac9de0ea76d3c83d803
Sekky61 <misa@majer.cz>
2021-09-27 20:09:34 +0200

    backend based on env.NODE_ENV

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -39,7 +39,8 @@ export default {
 	},
 	plugins: [
 		replace({
-			'process.env.NODE_ENV': process.env.NODE_ENV
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+			preventAssignment: true
 		}),
 		svelte({
 			compilerOptions: {
